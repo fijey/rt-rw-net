a9bf314ec2e34a67fabc95fa5d367d678d58de81
Fajar Mukti Hidayat <fajarmukti9@gmail.com>
2019-11-14 01:37:24 +0700

    ceate userpage, and user profile card

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,9 +6,12 @@ class User extends CI_Controller
     
     public function index() 
     {
+            $data['title'] = 'My Profile';
             $data['user']= $this->db->get_where('user', ['email' => 
             $this ->session->userdata('email')])->row_array();
-            echo 'selamat datang ' . $data['user']['name'];
+            
+            
+            $this->load->view('user/index', $data); 
     }
 
 }
