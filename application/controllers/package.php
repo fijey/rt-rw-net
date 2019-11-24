91564f34f4f9d3623e5938547d9649c50f4a416c
Fajar Mukti Hidayat <fajarmukti9@gmail.com>
2019-11-24 11:14:49 +0700

    menyelesaikan bug yang ada di submenu management

---
+++ application/controllers/package.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class package extends CI_Controller 
+{
+
+    public function index() {
+        $data['title'] = 'List Package';
+        $data['user']= $this->db->get_where('user', ['email' => 
+        $this ->session->userdata('email')])->row_array();
+        
+        
+        $this->load->view('templates/header', $data); 
+        $this->load->view('templates/sidebar', $data); 
+        $this->load->view('templates/topbar', $data); 
+        $this->load->view('package/index', $data); 
+        $this->load->view('templates/footer'); 
+
+    }
+
+}
---
+++ application/views/package/index.php
@@ -0,0 +1,127 @@
+       <link rel="stylesheet" href="style.css">
+       <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800"><?= $title;?></h1> 
+
+           <!-- pelayanan 1 -->
+           <div class="list-paket">
+          <h2>Paket Ramah</h2>
+          <div class="card-deck">
+            <div class="card">
+              <img class="card-img-top" src="images/10mbps.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Ramah 1</h5>
+                <p class="card-text">Paket Ini memberikan anda benefit : 
+                  <ul>
+                    <li>Speed 10/mbps</li>
+                    <li>Durasi 1jam</li>
+                    <li>Sisa waktu tidak dapat disimpan</li>
+                  </ul>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.10.000</label></a></small>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/15mbps.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Ramah 2</h5>
+                <p class="card-text">Paket Ini memberikan anda benefit
+                  <ul>
+                    <li>Speed 15/mbps</li>
+                    <li>Durasi 1jam</li>
+                    <li>Sisa waktu tidak dapat disimpan</li>
+                  </ul>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.15.000</label></a></small>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/20mbps.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Ramah 3</h5>
+                <p class="card-text">Paket Ini memberikan anda benefit </p>
+                <ul>
+                  <li>Speed 20/mbps</li>
+                  <li>Durasi 1jam</li>
+                  <li>Sisa waktu tidak dapat disimpan</li>
+                </ul>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.20.000</label></a></small>
+              </div>
+            </div>
+          </div>
+          </div>
+        <!-- akhir pelayanan -->
+
+        <!-- pelayanan 2 -->
+        <div class="list-paket">
+          <h2>Paket Gokil</h2>
+          <div class="card-deck">
+            <div class="card">
+              <img class="card-img-top" src="images/10mbps.jpg" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Gokil </h5>
+                <p class="card-text">Paket Ini memberikan anda benefit : 
+                  <ul>
+                    <li>Speed 10/mbps</li>
+                    <li>Durasi 1jam</li>
+                    <li>Sisa waktu dapat disimpan</li>
+                  </ul>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.20.000</label></a></small>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/15mbps.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Gokil</h5>
+                <p class="card-text">Paket Ini memberikan anda benefit
+                  <ul>
+                    <li>Speed 15/mbps</li>
+                    <li>Durasi 1jam</li>
+                    <li>Sisa waktu dapat disimpan</li>
+                  </ul>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.25.000</label></a></small>
+              </div>
+            </div>
+            <div class="card">
+              <img class="card-img-top" src="images/20mbps.png" alt="Card image cap">
+              <div class="card-body">
+                <h5 class="card-title">Paket Gokil</h5>
+                <p class="card-text">Paket Ini memberikan anda benefit </p>
+                <ul>
+                  <li>Speed 20/mbps</li>
+                  <li>Durasi 1jam</li>
+                  <li>Sisa waktu dapat disimpan</li>
+                </ul>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted"><a class="nav-item btn btn-primary tombol"><label style="color: white; padding-top: 2px;">Rp.30.000</label></a></small>
+              </div>
+            </div>
+          </div>
+          </div>
+        <!-- akhir pelayanan -->
+
+
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+     
